17ab3a43cbeb8bd998d245df6fbd6e3ce0641948
Shreyansh Sandilya <136112600+Sherry58@users.noreply.github.com>
2026-06-29 22:51:32 +0530

    Time: 11 ms (13.36%), Space: 12.2 MB (18.96%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -2,36 +2,39 @@ class Solution {
 public:
     vector<vector<string>> result;
     int N;
+    unordered_set<int> Cols;
+    unordered_set<int> Diag;
+    unordered_set<int> AntiDiag;
 
-    bool isvalid(vector<string>& board, int row, int col){
-        for(int i=row-1; i>=0; i--){
-            if(board[i][col] == 'Q') return false;
-        }
-        for(int i=row-1, j=col-1; i>=0 and j>=0; i--, j--){
-            if(board[i][j] == 'Q') return false;
-        }
-        for(int i=row-1, j=col+1; i>=0 and j<N; i--, j++){
-            if(board[i][j] == 'Q') return false;
-        }
-        return true;
-    }
-    void solve(vector<string>& board, int row){
-        if(row>=N){
+
+    void solve(vector<string>& board, int row) {
+        if (row >= N) {
             result.push_back(board);
             return;
         }
-        for(int col=0; col<N; col++){
-            if(isvalid(board, row, col)){
-                board[row][col] = 'Q';
-                solve(board,row+1);
-                board[row][col] = '.';
+        for (int col = 0; col < N; col++) {
+            int DiagConst = row+col;
+            int AntiDiagConst = row-col;
+
+            if(Cols.find(col)!=Cols.end() or Diag.find(DiagConst)!=Diag.end() or AntiDiag.find(AntiDiagConst)!=AntiDiag.end()){
+                continue;
             }
+            Cols.insert(col);
+            Diag.insert(DiagConst);
+            AntiDiag.insert(AntiDiagConst);
+            board[row][col] = 'Q';
+
+            solve(board, row+1);
+
+            Cols.erase(col);
+            Diag.erase(DiagConst);
+            AntiDiag.erase(AntiDiagConst);
+            board[row][col] = '.'; 
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        N=n;
-        vector<string> board(N, string(N,'.'));
-
+        N = n;
+        vector<string> board(N, string(N, '.'));
         solve(board, 0);
         return result;
     }
