70bb39773af4cc623b87e789b449d12ebd09c9cb
Shreyansh Sandilya <136112600+Sherry58@users.noreply.github.com>
2026-06-30 03:24:23 +0530

    Time: 203 ms (5%), Space: 239.9 MB (5%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -2,29 +2,41 @@ class Solution {
 public:
     vector<string> result;
     int N;
-    
-    void solve(string curr, int N, int open, int close){
+    bool isValid(string s) {
+        stack<char> st;
+        for(char ch : s){
+
+            if(ch == '(') st.push(ch);
+
+            else{ // yaha isiliye aaye ha kyunki ch = ')' hai
+                if(st.empty()) return false; //cannot push ')' into an empty stack , it would never find as pair and will always be an invalid parantheses.
+
+                if(st.top() == '(') st.pop();
+                else st.push(ch);
+            }
+        }
+        return st.empty();
+        
+    }
+    void solve(string curr, int N){
         if(curr.size() == 2*N){
-            result.push_back(curr);
+            if(isValid(curr)){
+                result.push_back(curr);
+            }
             return;
         }
-        if(open < N){
-            curr.push_back('(');
-            solve(curr,N, open+1, close);
-            curr.pop_back();
-        }
-        if(close < open){
-            curr.push_back(')');
-            solve(curr,N, open, close+1);
-            curr.pop_back();
-        }
+    
+        curr.push_back('(');
+        solve(curr,N);
+        curr.pop_back();
+
+        curr.push_back(')');
+        solve(curr,N);
+        curr.pop_back();
     }
      vector<string> generateParenthesis(int n) {
         N = n;
-        string curr = "";
-        int open = 0;
-        int close = 0;
-        solve(curr, n, open, close);
+        solve("", n);
         return result;
     }
 };
