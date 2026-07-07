8b9ff1232704f5d47aee24a1e41e1ac79496ab8d
Shreyansh Sandilya <136112600+Sherry58@users.noreply.github.com>
2026-07-07 12:52:11 +0530

    Time: 0 ms (100%), Space: 60.9 MB (52.91%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -11,45 +11,45 @@
  */
 class Solution {
 public:
-    vector<TreeNode*> res;
+    int galat = 0;
+    TreeNode* prev = NULL;
+    TreeNode* p_first = NULL, *p_second = NULL, *q_first = NULL, *q_second = NULL;
 
     void solve(TreeNode* root){
 
         if(root == NULL) return;
 
-        solve(root->left);
+       solve(root->left);
 
-        res.push_back(root);
+       if(prev == NULL) prev = root;
+
+       else{
+            if(prev->val >= root->val){
+
+                if(galat == 0){
+                    galat++;
+
+                    p_first = prev;
+                    p_second = root;
+
+                }
+
+                else{
+                    galat++;
+
+                    q_first = prev;
+                    q_second = root;
+                }
+            }
+
+            prev = root;
+        }
 
         solve(root->right);
     }
     void recoverTree(TreeNode* root) {
-        int galat = 0;
-        TreeNode* p_first = NULL, *p_second = NULL, *q_first = NULL, *q_second = NULL;
 
         solve(root);
-
-        for(int i = 0 ; i < res.size()-1; i++){
-
-            if(res[i]->val >= res[i+1]->val){
-
-                if(galat == 0){
-
-                    galat++;
-                    
-                    p_first = res[i];
-                    p_second = res[i+1];
-                }
-
-                else{
-
-                    galat++;
-
-                    q_first = res[i];
-                    q_second = res[i+1];
-                }
-            }
-        }
 
         if(galat == 1)  swap(p_first->val, p_second->val);
 
