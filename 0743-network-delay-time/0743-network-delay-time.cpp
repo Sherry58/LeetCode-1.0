3a8a8ebbb5fc1859a0fa162d9961fa2e103e5995
Shreyansh Sandilya <136112600+Sherry58@users.noreply.github.com>
2026-08-04 23:32:05 +0530

    Time: 77 ms (77.85%), Space: 44.2 MB (83.04%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -3,7 +3,7 @@ public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int,int>>> adj(n);
         for(auto &t : times){
-            int u = t[0]-1, v = t[1]-1, w = t[2];
+            int u = t[0]-1, v = t[1]-1, w = t[2]; //converted to zero based indexing
 
             adj[u].push_back({v, w});
         }
@@ -11,7 +11,7 @@ public:
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         vector<int> dist(n, INT_MAX);
 
-        k--;
+        k--; //converted to zero based indexing
         dist[k] = 0;
         pq.push({0, k});
 
@@ -34,9 +34,9 @@ public:
                 }
             }
         }
-        int res = *max_element(dist.begin(), dist.end());
+        int res = *max_element(dist.begin(), dist.end()); //to find max element in a vector using STL
 
-        if(res == INT_MAX) return -1;
+        if(res == INT_MAX) return -1; //matlab ki signal pohch hi nahi paya kisi ek ya ek se zyada node pe(unreachable node)
 
         return res;
         
