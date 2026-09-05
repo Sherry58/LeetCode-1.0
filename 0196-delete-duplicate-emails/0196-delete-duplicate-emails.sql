00baa0b005318b60818b7790464c81c2e7b4ecce
Shreyansh Sandilya <136112600+Sherry58@users.noreply.github.com>
2026-09-06 00:24:13 +0530

    Time: 247 ms (51.05%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -7,11 +7,18 @@
 -- )
 
 
-DELETE FROM Person
-WHERE id IN(
-    SELECT id FROM(
-        SELECT id, ROW_NUMBER() OVER(PARTITION BY email ORDER BY id) rn
-        FROM Person
-    ) t
-    WHERE rn > 1
-)
+-- DELETE FROM Person
+-- WHERE id IN(
+--     SELECT id FROM(
+--         SELECT id, ROW_NUMBER() OVER(PARTITION BY email ORDER BY id) rn
+--         FROM Person
+--     ) t
+--     WHERE rn > 1
+-- )
+
+DELETE
+FROM Person p1
+USING Person p2
+WHERE p1.email = p2.email
+AND 
+p1.id > p2.id
