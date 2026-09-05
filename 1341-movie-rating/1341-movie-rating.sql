bccc4ed3d9af5415376912f81702d237c788b5ff
Shreyansh Sandilya <136112600+Sherry58@users.noreply.github.com>
2026-09-05 17:18:51 +0530

    Time: 253 ms (65.35%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,25 @@
+-- Write your PostgreSQL query statement below
+SELECT name  AS results FROM(
+    SELECT u.name, COUNT(r.movie_id) AS cnt
+    FROM 
+    Users u LEFT JOIN MovieRating r
+    ON u.user_id = r.user_id
+    GROUP BY u.name
+    ORDER BY cnt DESC, u.name
+    LIMIT 1
+)
+
+UNION ALL
+
+SELECT title FROM(
+SELECT m.title, AVG(r.rating) AS avg_rating
+FROM
+Movies m LEFT JOIN MovieRating r
+ON m.movie_id = r.movie_id 
+WHERE created_at BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY m.movie_id,m.title
+ORDER BY avg_rating DESC,m.title
+LIMIT 1)
+
+
+
