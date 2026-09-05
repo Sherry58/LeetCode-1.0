9ee0fe789c4a65fe4dd2cff75cfe97fe007e40bf
Shreyansh Sandilya <136112600+Sherry58@users.noreply.github.com>
2026-09-05 10:47:27 +0530

    Time: 258 ms (75.81%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,11 +1,11 @@
 -- Write your PostgreSQL query statement below
 WITH temp AS(
     SELECT *, 
-        LAG(num,1) OVER(ORDER BY id) AS prev_num,
-        LAG(num,2) OVER(ORDER BY id) AS prev2_num
+        LEAD(num,1) OVER(ORDER BY id) AS next_num,
+        LEAD(num,2) OVER(ORDER BY id) AS next2_num
     FROM Logs
 )
 
 SELECT DISTINCT num AS ConsecutiveNums
 FROM temp
-WHERE num = prev_num AND prev_num = prev2_num
+WHERE num = next_num AND next_num = next2_num
